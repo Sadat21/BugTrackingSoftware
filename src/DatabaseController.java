254a2833c0075598663dc0107e0204d6e943cc5d
Harjee <harjee.johal@ucalgary.ca>
2017-11-16 15:03:38 -0700

    First Push!
    Added a controller skeleton

---
+++ src/DatabaseController.java
@@ -0,0 +1,71 @@
+/**
+ * This class is used to access the database (which is just a series of text files)
+ * Created by Harjee on 2017-11-16.
+ */
+public class DatabaseController {
+
+    public void isBugInDB(Bug checkedBug)
+    {
+
+    }
+
+    public void addBugToDB(Bug bugAdded)
+    {
+
+    }
+
+    public void removeBugFromDB(int bugID)
+    {
+
+    }
+
+    public void modifyBugInDB(int bugID)
+    {
+
+    }
+
+    public void isProductInDB(Product prodChecked)
+    {
+
+    }
+
+    public void addProductToDB(Product prodAdded)
+    {
+
+    }
+
+    public void removeProductFromDB(int prodID)
+    {
+
+    }
+
+    public void modifyProductInDB(int prodID, String updateInfo)
+    {
+
+    }
+
+    public void isDeveloperInDB(Developer checkedDev)
+    {
+
+    }
+
+    public void addDeveloperToDB(Developer addedDev)
+    {
+
+    }
+
+    public void removeDeveloperFromDB(int developerID)
+    {
+
+    }
+
+    public void modifyDeveloperInDB(int developerID, String updateInfo)
+    {
+
+    }
+
+    public boolean verifyLogin(String username, String password)
+    {
+        return false;
+    }
+}
